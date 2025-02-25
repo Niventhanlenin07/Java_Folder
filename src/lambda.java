3b189e96b0e280d4655217a730b3517bf0b6f2de
Niventhan Lenin <Niventhan.Lenin@mrcooper.com>
2025-02-26 01:43:12 +0530

    Updated

--- src/lambda.java
+++ src/lambda.java
@@ -9,14 +9,34 @@ import java.util.*;
 //         System.out.println(m.add(5, 7));
 //     }
 // }
-class lambda{
-    public static void main(String[] args) {
-        Integer[]arr={1,2,3,4,5,6};
-        List<Integer>list=new ArrayList<>(Arrays.asList(arr));
-        long s=list.stream()
-        .filter(n->n%2==0)
-        .mapToLong(n->n)
-        .count();
-        System.out.println(s);
-    }
-}
+// class lambda{
+//     public static void main(String[] args) {
+//         Integer[]arr={1,2,3,4,5,6};
+//         List<Integer>list=new ArrayList<>(Arrays.asList(arr));
+//         long s=list.stream()
+//         .filter(n->n%2==0)
+//         .mapToLong(n->n)
+//         .count();
+//         System.out.println(s);
+//     }
+// }
+// class demo{
+//     int n;
+//     String name;
+// }
+// class lambda{
+//     void method(demo n){
+//         n.n=20;
+//         n.name="Sur";
+//     }
+//     public static void main(String[] args) {
+//         demo d=new demo();
+//         d.n=10;
+//         System.out.println(d.n);
+//         System.out.println(d.name);
+//         lambda l=new lambda();
+//         l.method(d);
+//         System.out.println(d.n);
+//         System.out.println(d.name);
+//     }
+// }
