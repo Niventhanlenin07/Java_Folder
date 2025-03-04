b5dc37d69c54fb908f0f2bc7f4dfe46595f892ed
Niventhan Lenin <Niventhan.Lenin@mrcooper.com>
2025-03-04 21:17:59 +0530

    Updated

--- src/lambda.java
+++ src/lambda.java
@@ -40,11 +40,11 @@
 //         System.out.println(d.name);
 //     }
 // }
-class lambda{
-    public static void main(String[] args) {
-        int[]arr={1,2,3,4,5,6};
-        for(int a:arr){
-            System.out.println(a);
-        }
-    }
-}    
+// class lambda{
+//     public static void main(String[] args) {
+//         int[]arr={1,2,3,4,5,6};
+//         for(int a:arr){
+//             System.out.println(a);
+//         }
+//     }
+// }    
